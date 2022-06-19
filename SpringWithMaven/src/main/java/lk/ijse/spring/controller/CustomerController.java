1f7620122e60453c4a1e38a5195219dd65950585
CHARITH127 <charithrullz993@gmail.com>
2022-06-19 16:12:55 +0530

    Introducing model mapper

--- SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("customer")
-@CrossOrigin
+
 public class CustomerController {
 
     @Autowired
